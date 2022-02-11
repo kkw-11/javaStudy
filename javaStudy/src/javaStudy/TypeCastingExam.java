84d73f10c71e7bc124606a47956736deea7f5c72
kiwon <kkw-11@naver.com>
2022-02-11 19:02:15 +0900

    Java TypeCastingExam

---
+++ javaStudy/src/javaStudy/TypeCastingExam.java
@@ -0,0 +1,17 @@
+package javaStudy;
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 50000;
+		long y = x; // 묵시적 형변환
+		
+		
+		long x2 = 5;
+		int y2 = (int)x2; // 강제 형변환 : long타입에 있는 값을 int 타입에 담으려고 할 때 형변환을 해주어야 한다.
+		
+		
+	}
+
+}
