e42ad1bf2856e6c7981943e41fd8c9ea5db3634d
kiwon <kkw-11@naver.com>
2022-07-02 19:04:49 +0900

    javaStudy

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9c/00cec997d8f9001c1d9afc28ca2765a0
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaStudy/src/javaStudy/RefereneCataType.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class RefereneCataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 4;
+		String str = new String("hello");
+		
+		
+	}
+
+}
